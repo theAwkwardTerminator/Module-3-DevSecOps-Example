5fa55caf649193e2b924b7d2fb27df63d301377f
vic <6735550+theAwkwardTerminator@users.noreply.github.com>
2023-01-06 16:29:48 +0100

    fix security issues flagged by bandit

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ workflows = ['Container Scanning with Trivy', 'SAST with Bandit', 'Secrets scann
 
 
 def never_called(bla):
-    aws_access_token = "AKIALALEMEL33243OLIB"
-    os.subprocess.Popen('echo ${}'.format(aws_access_token), shell=True)
+    os.subprocess.Popen('echo ${}'.format(aws_access_token))
 
 
 def serve_image(state):
@@ -31,7 +30,7 @@ def hello_world():
 
 
 def main():
-    app.run(debug=True)
+    app.run()
 
 
 if __name__ == '__main__':
